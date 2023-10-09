7422f0109519eb48e516ca6f3ab431583bb67547
Erick Barrera <dannbj1@gmail.com>
2023-10-09 17:23:35 -0600

    Update atributos y correción de errores

--- Usuario.java
+++ Usuario.java
@@ -7,40 +7,48 @@
  * 
  * @author: Luisa Jiménez - 23011, Erick Barrera - 231238
  * @date: 08/10/2023
- * @version: 1.0.1
- * @lastUpdate: 08/10/2023
- * Cambios realizados: corrección de errores
+ * @version: 1.0.2
+ * @lastUpdate: 09/10/2023
+ * Cambios realizados: correción de errores y cambios a atributos
  * 
  */
 
 import java.util.ArrayList;
+import java.util.HashMap;
 public class Usuario {
 
-    private String nombre;
-    private String correo;
-    private int edad;
-    private String sexo;
-    private String ciudad;
+    private String nombre, correo, password, sexo, ciudad;
+    private int edad, diasSobrio;
     private int[] frecuenciaConsumo;
-    private ArrayList<String> objetivos;
-    private ArrayList<String> reflexiones;
-    private ArrayList<String> sintomas;
-    private int diasSobrio;
+    private ArrayList<String> objetivos, reflexiones, sintomas;
+    private HashMap<String, String> contactosEmergencia;
     private Contador contador;
-    private Habito habito;
+    private Habito habitos;
 
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public int getEdad() {
         return edad;
     }
-
     public void setEdad(int edad) {
         this.edad = edad;
     }
@@ -48,7 +56,6 @@ public class Usuario {
     public String getSexo() {
         return sexo;
     }
-
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
@@ -56,44 +63,82 @@ public class Usuario {
     public String getCiudad() {
         return ciudad;
     }
-
     public void setCiudad(String ciudad) {
         this.ciudad = ciudad;
     }
 
-    public int[] getConsumo() {
+    public int[] getFrecuenciaConsumo() {
         return frecuenciaConsumo;
     }
-
-    public void setConsumo(int[] frecuenciaConsumo) {
+    public void setFrecuenciaConsumo(int[] frecuenciaConsumo) {
         this.frecuenciaConsumo = frecuenciaConsumo;
     }
 
-    public String[] getObjetivos() {
-        return objetivos.toArray(new String[0]);
+    public ArrayList<String> getObjetivos() {
+        return objetivos;
+    }
+    public void setObjetivos(ArrayList<String> objetivos) {
+        this.objetivos = objetivos;
     }
 
-    public void setObjetivos(String objetivo) {
-        this.objetivos.add(objetivo);
+    public Habito getHabitos() {
+        return habitos;
+    }
+    public void setHabitos(Habito habitos) {
+        this.habitos = habitos;
+    }
+
+    public HashMap<String, String> getContactosEmergencia() {
+        return contactosEmergencia;
+    }
+    public void setContactosEmergencia(HashMap<String, String> contactosEmergencia) {
+        this.contactosEmergencia = contactosEmergencia;
+    }
+
+    public ArrayList<String> getSintomas() {
+        return sintomas;
+    }
+    public void setSintomas(ArrayList<String> sintomas) {
+        this.sintomas = sintomas;
+    }
+
+    public ArrayList<String> getReflexiones() {
+        return reflexiones;
+    }
+    public void setReflexiones(ArrayList<String> reflexiones) {
+        this.reflexiones = reflexiones;
     }
 
     public int getDiasSobrio() {
         return diasSobrio;
     }
-
     public void setDiasSobrio(int diasSobrio) {
         this.diasSobrio = diasSobrio;
     }
 
-    public Usuario(String nombre, int edad, String sexo, String ciudad, int[] frecuenciaConsumo, ArrayList<String> objetivos, int diasSobrio, Contador contador, Habito habito) {
+    public Contador getContador() {
+        return contador;
+    }
+    public void setContador(Contador contador) {
+        this.contador = contador;
+    }
+
+    public Usuario(String nombre, String correo, String password, int edad, String sexo, String ciudad, int[] frecuenciaConsumo, 
+    ArrayList<String> objetivos, HashMap<String, String> contactosEmergencia, ArrayList<String> reflexiones, ArrayList<String> sintomas,
+     int diasSobrio, Contador contador, Habito habitos) {
         this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
         this.edad = edad;
         this.sexo = sexo;
         this.ciudad = ciudad;
         this.frecuenciaConsumo = frecuenciaConsumo;
         this.objetivos = objetivos;
+        this.habitos = habitos;
+        this.contactosEmergencia = contactosEmergencia;
+        this.reflexiones = reflexiones;
+        this.sintomas = sintomas;
         this.diasSobrio = diasSobrio;
         this.contador = contador;
-        this.habito = habito;
     }
 }
